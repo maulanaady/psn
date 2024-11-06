d1910b018021cfddd743dee57aad989d2d59d09d
maulanaady <adysulton@gmail.com>
2024-11-06 09:12:57 +0700

    Add db_cleanup DAG

---
+++ airflow/dags/airflow_db_cleanup.py
@@ -0,0 +1,42 @@
+"""
+A maintenance workflow that you can deploy into Airflow to periodically clean
+out the task logs to avoid those getting too big.
+"""
+
+import os
+from datetime import timedelta
+import pendulum
+import jinja2
+from default.default_config import DefaultDAG
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow.operators.empty import EmptyOperator
+
+default_args = {
+    "owner": "operations",
+    "depends_on_past": False,
+    "retries": 3,
+    "retry_delay": timedelta(minutes=1),
+}
+
+with DefaultDAG(
+    dag_id=os.path.basename(__file__).replace(".pyc", "").replace(".py", ""),
+    schedule="@daily",
+    start_date=pendulum.datetime(2024, 5, 27, tz="Asia/Jakarta"),
+    catchup=False,
+    tags=["airflow-maintenance-dags"],
+    template_undefined=jinja2.Undefined,
+) as dag:
+    if hasattr(dag, "doc_md"):
+        dag.doc_md = __doc__
+    if hasattr(dag, "catchup"):
+        dag.catchup = False
+
+    start = EmptyOperator(task_id="start", dag=dag)
+
+    db_cleanup_op = SSHOperator(
+        task_id="db_cleanup",
+        ssh_conn_id="host-airflow",
+        command='docker exec airflow-worker /bin/bash -c "/opt/airflow/include/script/db_cleanup.sh {{ ts }}"',
+    )
+
+    start >> db_cleanup_op
